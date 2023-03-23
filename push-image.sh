6668efaf34edb38101c4116fe0e2e7c16aa0a75d
samm@msdemos.kiwi <sam.mackenzie@datacom.co.nz>
2023-03-23 03:03:35 +0000

    update domain

--- push-image.sh
+++ push-image.sh
@@ -3,8 +3,6 @@
 DOCKER_BUILDKIT=1
 
 docker image build . -f packages/backend/Dockerfile --tag backstage --build-arg CACHEBUST=$(date +%s)
-
 #docker tag backstage samdevreg.azurecr.io/backstage
-
 #docker push samdevreg.azurecr.io/backstage
 
